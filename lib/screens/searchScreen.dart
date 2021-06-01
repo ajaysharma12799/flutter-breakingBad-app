8b888fee7d6408cad35c7cd4c94fa59bc32eff69
ajaysharma12799 <ajaysharma12799@gmail.com>
2021-06-02 00:36:37 +0530

    Completed Search Functionality

--- lib/screens/searchScreen.dart
+++ lib/screens/searchScreen.dart
@@ -1,7 +1,7 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_breakingbad_app/models/characterModel.dart'
     show Character;
-import 'package:flutter_breakingbad_app/widgets/characterCard.dart';
 import 'package:http/http.dart' as http;
 import '../API.dart' show API;
 import 'dart:convert';
@@ -17,21 +17,37 @@ class _SearchScreenState extends State<SearchScreen> {
   GlobalKey<FormState> _formKey = GlobalKey();
   List<Character> breakingBadCharacters = [];
 
-  Future<void> getSingleCharacter(String characterName) async {
+  Future<List<Character>> getSingleCharacter(String characterName) async {
     final response =
         await http.get(Uri.parse("${API}/characters?name=${characterName}"));
     List jsonResponse = json.decode(response.body);
 
     if (response.statusCode == 200) {
-      // return jsonResponse
-      //     .map((element) => Character.fromJson(element))
-      //     .toList();
-      for (var singleCharacter in jsonResponse) {
-        breakingBadCharacters.add(singleCharacter);
-      }
+      return jsonResponse
+          .map((element) => Character.fromJson(element))
+          .toList();
     } else {
       throw Exception("Failed To Load Characters");
     }
+  }
+
+  void _showModalSheet() {
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext context) {
+          return Container(
+            height: 300,
+            margin: EdgeInsets.all(20),
+            padding: EdgeInsets.all(10.0),
+            child: Text(
+              "Note :- This Search Screen Page Show All Character Who are Present in Breaking Bad TV Series and You Can Also Search For Specific Characters \n\nPlease Forgive Me If My Design Is Not Perfect, I am Trying to Improve MySelf on Daily Basis.",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          );
+        });
   }
 
   void submit() async {
@@ -42,152 +58,136 @@ class _SearchScreenState extends State<SearchScreen> {
     }
   }
 
-  Widget buildSwitches() {
-    return Container(
-      width: 400,
-      child: Column(
-        children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                "Characters",
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Switch(
-                value: true,
-              ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                "Episodes",
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Switch(
-                value: false,
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
+  @override
+  void dispose() {
+    textController.dispose(); // Disposing TextEditingController
+    super.dispose();
   }
-
-  // Widget buildCharacterViewWidget() {
-  //   return Container(
-  //     width: 350,
-  //     child: ListView.builder(
-  //       itemCount: badCharacters.length,
-  //       itemBuilder: (_, index) {
-  //         return CharacterCard(
-  //           characterName: badCharacters[index][index].name,
-  //           imageURL: badCharacters[index].imageURL,
-  //           birthday: badCharacters[index].birthDate,
-  //           occupation: badCharacters[index].occupation,
-  //         );
-  //       },
-  //     ),
-  //   );
-  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 5,
-        centerTitle: true,
-        title: Text(
-          "Breaking Bad",
-          style: TextStyle(
-            fontWeight: FontWeight.w400,
-            fontSize: 20,
-            color: Colors.white,
+        appBar: AppBar(
+          elevation: 5,
+          centerTitle: true,
+          title: Text(
+            "Breaking Bad",
+            style: TextStyle(
+              fontWeight: FontWeight.w400,
+              fontSize: 20,
+              color: Colors.white,
+            ),
           ),
         ),
-      ),
-      body: Container(
-        child: Column(
-          children: <Widget>[
-            Container(
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.blue,
+        body: Container(
+          child: Column(
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.blue,
+                  ),
+                  borderRadius: BorderRadius.circular(50.0),
                 ),
-                borderRadius: BorderRadius.circular(50.0),
-              ),
-              margin: EdgeInsets.only(
-                top: 20,
-                bottom: 20,
-                left: 10,
-                right: 10,
-              ),
-              child: Form(
-                key: _formKey,
-                child: TextFormField(
-                  controller: textController,
-                  cursorColor: Colors.black,
-                  validator: (String value) {
-                    if (value.isEmpty) return "Please Enter Something";
-                  },
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: "Search Character or Episode",
-                    contentPadding: EdgeInsets.all(20),
-                    hintStyle: TextStyle(
-                      fontSize: 18,
+                margin: EdgeInsets.only(
+                  top: 20,
+                  bottom: 20,
+                  left: 10,
+                  right: 10,
+                ),
+                child: Form(
+                  key: _formKey,
+                  child: TextFormField(
+                    controller: textController,
+                    cursorColor: Colors.black,
+                    validator: (String value) {
+                      if (value.isEmpty) return "Please Enter Something";
+                    },
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Search Character",
+                      contentPadding: EdgeInsets.all(20),
+                      hintStyle: TextStyle(
+                        fontSize: 18,
+                      ),
+                      suffixIcon: Icon(Icons.search),
                     ),
-                    suffixIcon: Icon(Icons.search),
                   ),
                 ),
               ),
-            ),
-            Container(
-              width: 400,
-              child: ElevatedButton(
-                onPressed: () => submit(),
-                child: Text(
-                  "Search",
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(
-                    Color.fromRGBO(255, 74, 74, 1),
-                  ),
-                  foregroundColor: MaterialStateProperty.all(Colors.white),
-                ),
-              ),
-            ),
-            Expanded(
-              child: SizedBox(
-                height: 200.0,
-                child: ListView.builder(
-                  itemCount: breakingBadCharacters.length,
-                  itemBuilder: (_, index) {
-                    return CharacterCard(
-                      characterName: breakingBadCharacters[index].name,
-                      imageURL: breakingBadCharacters[index].imageURL,
-                      birthday: breakingBadCharacters[index].birthDate,
-                      occupation: breakingBadCharacters[index].occupation,
-                    );
+              Container(
+                width: 400,
+                child: ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      submit();
+                    });
                   },
+                  child: Text(
+                    "Search",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(
+                      Color.fromRGBO(255, 74, 74, 1),
+                    ),
+                    foregroundColor: MaterialStateProperty.all(Colors.white),
+                  ),
                 ),
               ),
-            ),
-          ],
+              Expanded(
+                child: SizedBox(
+                  height: 150.0,
+                  child: FutureBuilder(
+                    future: getSingleCharacter(textController.text),
+                    builder: (_, snapshot) {
+                      return snapshot.hasData
+                          ? ListView.builder(
+                              itemCount: snapshot.data.length,
+                              itemBuilder: (_, index) {
+                                return Card(
+                                  child: Stack(
+                                    children: <Widget>[
+                                      CachedNetworkImage(
+                                        imageUrl: snapshot.data[index].imageURL,
+                                      ),
+                                      Positioned(
+                                        bottom: 0,
+                                        child: Container(
+                                          width: 430,
+                                          height: 50,
+                                          color: Colors.black54,
+                                          padding: EdgeInsets.all(5.0),
+                                          child: Text(
+                                            snapshot.data[index].name,
+                                            style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 18,
+                                            ),
+                                            textAlign: TextAlign.center,
+                                          ),
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                );
+                              },
+                            )
+                          : Text("Search For Character");
+                    },
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
-    );
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: _showModalSheet,
+          label: Text("Please Open Me First"),
+          backgroundColor: Colors.green,
+        ));
   }
 }
